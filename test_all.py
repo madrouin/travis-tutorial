96acfa0f91708811ef75382c16f5ec11b36dda9c
marc-antoine drouin <marc-antoine.drouin@lmd.polytechnique.fr>
2017-05-31 12:18:50 +0200

    add cache to travis

--- test_all.py
+++ test_all.py
@@ -2,14 +2,18 @@ from __future__ import division
 
 import numpy as np
 
+
 def test_addition():
     assert 1 + 1 == 2
+
 
 def test_multiplication():
     assert 4*5 == 20
 
+
 def test_division():
     assert 3/2 == 1.5
+
 
 def test_partition():
     a = np.array([3, 4, 2, 1])
