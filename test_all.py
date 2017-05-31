e3859277768f4321db55a37db0486e4c58d60ca7
marc-antoine drouin <marc-antoine.drouin@lmd.polytechnique.fr>
2017-05-31 11:55:34 +0200

    add test for partition

--- test_all.py
+++ test_all.py
@@ -1,5 +1,7 @@
 from __future__ import division
 
+import numpy as np
+
 def test_addition():
     assert 1 + 1 == 2
 
@@ -8,3 +10,7 @@ def test_multiplication():
 
 def test_division():
     assert 3/2 == 1.5
+
+def test_partition():
+    a = np.array([3, 4, 2, 1])
+    assert np.partition(a, 3) == np.array([2, 1, 3, 4])
